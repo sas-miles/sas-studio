ccdd5a9bd91af94e4de6283f3750a287961a541b
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-12-30 09:59:49 -0500

    media permissions issues

--- next.config.js
+++ next.config.js
@@ -1,23 +1,27 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_URL // Automatically provided in Vercel
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+      ...(NEXT_PUBLIC_SERVER_URL
+        ? [NEXT_PUBLIC_SERVER_URL].map((item) => {
+            const url = new URL(item)
+            return {
+              hostname: url.hostname,
+              protocol: url.protocol.replace(':', ''),
+            }
+          })
+        : []),
+      {
+        hostname: 'vvbsxhulglwtgatwygut.supabase.co', // Replace with your Supabase storage domain
+        protocol: 'https',
+      },
     ],
   },
   reactStrictMode: true,
